601016fa0629762fdcffa5ddbc5d2c95b5539e97
njl07 <akanass@icloud.com>
2018-11-02 15:49:36 +0100

    release(version): v7.0.0

--- projects/custom-elements-loader/src/lib/elements-loader.module.ts
+++ projects/custom-elements-loader/src/lib/elements-loader.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ElementsLoaderService } from '@hapiness/ng-elements-loader';
 
 @NgModule({
-    imports: [ BrowserAnimationsModule ],
-    providers: [ ElementsLoaderService ]
+    imports: [ BrowserAnimationsModule ]
 })
-export class ElementsLoaderModule {
-    ngDoBootstrap() {
+export class ElementsLoaderModule implements DoBootstrap {
+    constructor(private _elementsLoaderService: ElementsLoaderService) {}
+    ngDoBootstrap(appRef: ApplicationRef): void {
     }
 }
